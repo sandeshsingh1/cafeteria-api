f5d2b2c8ae3c1f934b3ce523cf778aaea0f68221
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-03-19 22:16:53 +0530

    Added urls file

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem, Order
+from .serializers import MenuItemSerializer, OrderSerializer
 
-# Create your views here.
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['category', 'name']
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
